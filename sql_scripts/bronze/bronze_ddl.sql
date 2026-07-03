45fb639399d11ea13b4c9129ec7519e1eeeeff1b
kouppis7-prog <kouppis7@gmail.com>
2026-07-03 11:40:22 +0300

    Create bronze_layer tables with drop if exists
    This script creates multiple tables in the 'bronze_layer' schema, ensuring any existing tables are dropped before creation.

---
+++ sql_scripts/bronze/bronze_ddl.sql
@@ -0,0 +1,76 @@
+/* 
+    This script creates tables in the 'bronze_layer' schema, dropping existing tables 
+    if they already exist.
+*/
+
+USE DataWarehouse
+GO
+
+IF OBJECT_ID ('bronze_layer.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze_layer.crm_cust_info;
+GO
+CREATE TABLE bronze_layer.crm_cust_info(
+	cst_id INT,
+	cst_key VARCHAR(30),
+	cst_firstname NVARCHAR(30),
+	cst_lastname NVARCHAR(30),
+	cst_marital_status NVARCHAR(5),
+	cst_gender NVARCHAR(5),
+	cst_create_date DATE
+);
+
+IF OBJECT_ID ('bronze_layer.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE bronze_layer.crm_prd_info;
+GO
+CREATE TABLE bronze_layer.crm_prd_info(
+	prd_id INT,
+	prd_key NVARCHAR(30),
+	prd_nm NVARCHAR(50),
+	prd_cost INT,
+	prd_line NVARCHAR(10),
+	prd_start_date DATE,
+	prd_end_dt DATE
+);
+
+IF OBJECT_ID ('bronze_layer.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE bronze_layer.crm_sales_details;
+GO
+CREATE TABLE bronze_layer.crm_sales_details(
+	sls_ord_num NVARCHAR(30),
+	sls_prod_key NVARCHAR(30),
+	sls_cust_id INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
+);
+
+IF OBJECT_ID ('bronze_layer.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE bronze_layer.erp_cust_az12;
+GO
+CREATE TABLE bronze_layer.erp_cust_az12(
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(30)
+);
+
+IF OBJECT_ID ('bronze_layer.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE bronze_layer.erp_loc_a101;
+GO
+CREATE TABLE bronze_layer.erp_loc_a101(
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
+);
+
+
+IF OBJECT_ID ('bronze_layer.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE bronze_layer.erp_px_cat_g1v2;
+GO
+CREATE TABLE bronze_layer.erp_px_cat_g1v2(
+	id NVARCHAR(50),
+	cat NVARCHAR(30),
+	subcat NVARCHAR(30),
+	maintenance NVARCHAR(20)
+);
